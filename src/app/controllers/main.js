3010d7b903e05834a83e252f500929147b95f605
schenkfab <schenkfab@users.noreply.github.com>
2016-05-11 18:25:43 +0200

    feat: data is now filtered

--- src/app/controllers/main.js
+++ src/app/controllers/main.js
@@ -39,55 +39,86 @@ angular.module('myApp').controller('issueCtrl', function($scope, $http) {
 					['2016/04', 124, 180, 18],
 					['2016/05', 150, 50, 3]];
 
-	$http.get('http://localhost:3000/assets/mock/issues.csv').success(function(allText) {
+	function getData(cb) {
+		$http.get('http://localhost:3000/assets/mock/issues.csv').success(function(allText) {
+			// split content based on new line
+			var allTextLines = allText.split(/\r\n|\n/);
+			var headers = allTextLines[0].split(',');
+			var lines = [];
 
-		// split content based on new line
-		var allTextLines = allText.split(/\r\n|\n/);
-		var headers = allTextLines[0].split(',');
-		var lines = [];
-
-		for ( var i = 0; i < allTextLines.length; i++) {
-			// split content based on comma
-			var data = allTextLines[i].split(',');
-			if (data.length == headers.length) {
-				var tarr = [];
-				for ( var j = 0; j < headers.length; j++) {
-					tarr.push(data[j]);
-				}
-				lines.push(tarr);
-			}
-		}
-		lines.forEach(function(l) {
-			if (l[0] != 'id') {
-				l[0] = parseInt(l[0]);
-				l[1] = new Date(l[1]);
-				l[2] = new Date(l[2]);
-				if (l[6] == 'Open') {
-					l[2] = null;
+			for ( var i = 0; i < allTextLines.length; i++) {
+				// split content based on comma
+				var data = allTextLines[i].split(',');
+				if (data.length == headers.length) {
+					var tarr = [];
+					for ( var j = 0; j < headers.length; j++) {
+						tarr.push(data[j]);
+					}
+					lines.push(tarr);
 				}
 			}
-		});
-		$scope.filters = {};
-		$scope.issue.header = lines[0];
-		$scope.issue.values = {};
-		// get values for all headers:
-		$scope.issue.header.forEach(function(value, index) {
-			$scope.issue.values[value] = [];
 			lines.forEach(function(l) {
 				if (l[0] != 'id') {
-					if(value == 'created' || value == 'closed') {
-						if ($scope.issue.values[value].map(Number).indexOf(+l[index]) == -1) {
-							$scope.issue.values[value].push(l[index]);
-						} 
-					} else {
-						if ($scope.issue.values[value].indexOf(l[index]) == -1) {
-							$scope.issue.values[value].push(l[index]);
-						} 
+					l[0] = parseInt(l[0]);
+					l[1] = new Date(l[1]);
+					l[2] = new Date(l[2]);
+					if (l[6] == 'Open') {
+						l[2] = null;
 					}
 				}
 			});
-			console.log($scope.issue.values);
+			$scope.filters = {};
+			$scope.issue.header = lines[0];
+			$scope.issue.values = {};
+			// get values for all headers:
+			$scope.issue.header.forEach(function(value, index) {
+				$scope.issue.values[value] = [];
+				lines.forEach(function(l) {
+					if (l[0] != 'id') {
+						if(value == 'created' || value == 'closed') {
+							if ($scope.issue.values[value].map(Number).indexOf(+l[index]) == -1) {
+								$scope.issue.values[value].push(l[index]);
+							} 
+						} else {
+							if ($scope.issue.values[value].indexOf(l[index]) == -1) {
+								$scope.issue.values[value].push(l[index]);
+							} 
+						}
+					}
+				});
+			});
+			cb(lines);
 		});
-		$scope.issue.data = lines;
+
+	}
+
+
+	$scope.applyFilter = function() {
+		var originalData = $scope.issue.data;
+		$scope.filteredData = [];
+		var i = originalData.length;
+		while (i--) {
+			console.log(i);
+			if (originalData[i][0] != 'id') {
+				$scope.issue.header.forEach(function(head, index) {
+					if ($scope.filters[head]) {
+						if (originalData[i][index] != $scope.filters[head]) {
+							$scope.issue.data.splice(i, 1);
+						}
+					}
+				});
+			}
+		}	
+	};
+
+	$scope.reloadData = function() {
+		getData(function(data) {
+			$scope.issue.data = data;
+		});
+	};
+
+	getData(function(data) {
+		$scope.issue.data = data;
 	});
+	
 });
